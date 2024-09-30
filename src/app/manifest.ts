ea469da38cd98055d18fa8d8a9a2905218de50ed
Levente Botos <botos.levente2007@gmail.com>
2024-09-30 19:02:14 +0200

    now

---
+++ src/app/manifest.ts
@@ -0,0 +1,19 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+   name: "Az Európai Únió Főbb Szervei",
+  short_name: "Az Európai Únió Főbb Szervei",
+  background_color: "#003399",
+  theme_color: "#003399",
+  display: "fullscreen",
+  scope: "/",
+  start_url: "/",
+  icons: [
+    {
+      "src": "/logo.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ]}
+}
